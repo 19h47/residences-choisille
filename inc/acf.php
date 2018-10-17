10de5f6fe97beb2bda6f6088f351e2035195cecd
Jérémy Levron <jeremylevron@19h47.fr>
2018-10-18 00:35:09 +0200

    ⬆️ Update dependencies

--- inc/acf.php
+++ inc/acf.php
@@ -4,7 +4,7 @@ add_filter('acf/prepare_field/name=event_color', 'change_acf_event_color_picker'
 
 /**
  * Change ACF color picker for pot
- * 
+ *
  * @param  $field
  * @return $field
  */
@@ -14,15 +14,15 @@ function change_acf_event_color_picker( $field ) {
 	?><script>
 
 		acf.add_filter('color_picker_args', function( args, $field ){
-	
+
 			// do something to args
 			args.palettes = ['#6666d5', '#343471'];
-	
+
 			// return
 			return args;
-		
+
 		});
-		
+
 	</script><?php
 
 	return $field;
@@ -34,7 +34,7 @@ add_filter('acf/prepare_field/name=page_color_secondary', 'change_acf_page_color
 
 /**
  * Change ACF color picker for post
- * 
+ *
  * @param  $field
  * @return $field
  */
@@ -44,27 +44,45 @@ function change_acf_page_color_picker( $field ) {
 	?><script>
 
 		acf.add_filter('color_picker_args', function( args, $field ){
-	
+
 			// do something to args
 			args.palettes = [
-				'#6666d5', 
-				'#343471', 
-				'#55cdb9', 
-				'#009696', 
-				'#965fb4', 
-				'#6E3278', 
-				'#ffc80a', 
+				'#6666d5',
+				'#343471',
+				'#55cdb9',
+				'#009696',
+				'#965fb4',
+				'#6E3278',
+				'#ffc80a',
 				'#FF960A',
 				'#e6d7eb',
-				'#d2f0eb' 
+				'#d2f0eb'
 			];
-	
+
 			// return
 			return args;
-		
+
 		});
-		
+
 	</script><?php
 
 	return $field;
+}
+
+add_filter('acf/settings/save_json', 'feh_acf_json_save_point');
+
+/**
+ * feh_acf_json_save_point
+ *
+ * @param   $path
+ * @see     https://www.advancedcustomfields.com/resources/local-json/
+ * @return  $path
+ */
+function feh_acf_json_save_point( $path ) {
+
+    // update path
+    $path = get_stylesheet_directory() . '/acf-export';
+
+    // return
+    return $path;
 }
